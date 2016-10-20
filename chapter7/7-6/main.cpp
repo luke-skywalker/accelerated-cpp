17d55347d7fc0832c2d010c80f15d550e98a0dff
Luca <manzariluca@gmail.com>
2016-10-20 20:25:30 +0200

    7-6 done!

--- chapter7/7-6/main.cpp
+++ chapter7/7-6/main.cpp
@@ -1,13 +1,37 @@
 // reimplementation of 7-0-2, without recursion
 
 /*
-  a Grammar is a description specifying how to create sentences
+  a Grammar is a description specifying how to create sentences;
+            it is a map mapping a string to a Rule collection
 
-  For example, "the <noun-phrase> <verb> <location>" is a grammar.
+	    <noun>        [ [ "cat" ],
+	                    [ "dog" ],
+			    [ "johnny" ],
+			    [ "jb", "hankamanka", "junior" ] ]
+
+	    <verb>        [ [jumps],
+	                    [laughs],
+			    [prays] ]
+
+	    <location>    [ [ "on", "the", "table" ] ]
+
+	    <sentence>    [ [the <noun> <verb> <location>],
+	                    [the <noun> <verb>] ]
+            __________    _______________________________________________________
+	     ^             ^
+	     string        Rule collection
+
+  a Rule collection is a vector of Rules
+
+  a Rule is a vector of strings;
+         it can have only one string, like [ "cat" ]
+	 or more than one string, like [ "on", "the", "table" ]
+	                            or [ "jb", "hankamanka", "junior" ]
 
 */
 
 #include <cstdlib>
+#include <ctime>     // providing a seed for the prand number generator
 #include <iostream>
 #include <istream>
 #include <iterator>
@@ -49,70 +73,62 @@ int nrand(int n)
   return r;
 }
 
-// void gen_aux(const Grammar& g,
-// 	     const std::string& word,
-// 	     std::vector<std::string>& ret)
-// {
-//   // is the word not bracketed?
-//   // if the word is not bracketed, it represents itself: so we can
-//   // append it to ret and we're done
-//   if (!bracketed(word)) {
-//     ret.push_back(word);
-//   } else {
-//     // locate the rule that corresponds to word
-//     // std::map.find() searches through the keys of the map
-//     Grammar::const_iterator it = g.find(word);
-//     // did we not have a rule related to that word?
-//     if (it == g.end())
-//       throw std::logic_error("empty rule");
-
-//     // fetch the set of possible rules...
-//     //
-//     // that is if "it" is our iterator, then **for example**
-//     //    it->first points to <name>
-//     //    it->second points to Luca
-//     const Rule_collection& c = it->second;
-
-//     // ...from which we select one at random
-//     const Rule& r = c[nrand(c.size())];
-
-//     // recursively expand the selected rule
-//     for (Rule::const_iterator i = r.begin(); i != r.end(); ++i)
-//       gen_aux(g, *i, ret);
-//   }
-// }
-
 void gen_aux(const Grammar& g,
 	     const std::string& word,
 	     std::vector<std::string>& ret)
 {
-  // is the word not bracketed?
-  // if the word is not bracketed, it represents itself: so we can
-  // append it to ret and we're done
-  if (!bracketed(word)) {
-    ret.push_back(word);
-  } else {
-    // locate the rule that corresponds to word
-    // std::map.find() searches through the keys of the map
-    Grammar::const_iterator it = g.find(word);
-    // did we not have a rule related to that word?
-    if (it == g.end())
-      throw std::logic_error("empty rule");
+  // This function is being modified not to use any recursive call.
+  // Two vectors are used:
+  // * one will hold the fully unwound, generated sentence
+  // * the other will hold the rules and will be used as a stack.
+  Rule rule_stack;
 
-    // fetch the set of possible rules...
-    //
-    // that is if "it" is our iterator, then **for example**
-    //    it->first points to <name>
-    //    it->second points to Luca
-    const Rule_collection& c = it->second;
+  // fetch the sentence (assumed one has been input correctly)
+  Grammar::const_iterator it = g.find(word);
+  if (it == g.end())
+    throw std::logic_error("empty rule");
+  const Rule_collection& c = it->second;
+  // select (the/a) sentence
+  const Rule& r = c[nrand(c.size())];
 
-    // ...from which we select one at random
-    const Rule& r = c[nrand(c.size())];
+  // store it as a vector of string in ret
+  for (Rule::const_iterator i = r.begin(); i != r.end(); ++i)
+    ret.push_back(*i);
 
-    // recursively expand the selected rule
-    for (Rule::const_iterator i = r.begin(); i != r.end(); ++i)
-      gen_aux(g, *i, ret);
-  }
+  // check for things that need to be expanded
+  bool needToExpand = true;
+  int pos; // position of the bracketed words
+
+  while (needToExpand)
+    {
+      int check = 0;
+      Rule::const_iterator i;
+      for (i = ret.begin(); i != ret.end(); ++i)
+	{
+	  rule_stack.clear();
+	  if (bracketed(*i))
+	    {
+	      // expand bracketed things in the stack
+	      it = g.find(*i);
+	      if (it == g.end())
+		throw std::logic_error("empty rule");
+	      rule_stack = it->second[nrand(c.size())];
+	      // get out of the loop, remembering where we are
+	      pos = i - ret.begin();
+	      break;
+	    }
+	}
+
+      ret.erase(ret.begin() + pos);
+      ret.insert(ret.begin() + pos, rule_stack.begin(), rule_stack.end());
+
+      for (i = ret.begin(); i != ret.end(); ++i)
+	if (bracketed(*i))
+	  check += 1;
+
+      if (check == 0)
+	needToExpand = false;
+    }
 }
 
 
@@ -137,13 +153,18 @@ Grammar read_grammar(std::istream& in)
     std::vector<std::string> entry = split(line);
     if (!entry.empty())
       // use the category to store the associated rule
-      ret[entry[0]].push_back(Rule(entry.begin() + 1, entry.end()));
+      ret[entry[0]].push_back(
+			      Rule( entry.begin() + 1, entry.end() )
+			      );
   }
   return ret;
 }
 
 int main()
 {
+  // seed the pseudo-random number generator
+  std::srand(std::time(0));
+
   // generate the sentence
   std::vector<std::string> sentence = gen_sentence(read_grammar(std::cin));
 
@@ -159,6 +180,7 @@ int main()
     std::cout << " " << *it;
     ++it;
   }
+
   std::cout << std::endl;
   return 0;
 }
